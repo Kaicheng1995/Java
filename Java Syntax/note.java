d2dd8816092562256aef65d9bca37ded330f7265
Kaicheng Jia <kaicheng.jia@berkeley.edu>
2021-10-04 00:15:23 -0700

    leetcode day4

---
+++ Java Syntax/note.java
@@ -0,0 +1,18 @@
+
+// ? : 的使用，如果成立选A，不成立选B
+判断条件？A：B
+
+// /符号代表出法向下取整，整数/整数只会得到整数，如果全部用float可以得到小数
+(float)3/(float)4
+
+// 当pointer == null 时，指针已经不在了，重新赋值和原指针位置已经没有关系了！
+
+// 遍历时担心nullpointer的话，可以创建一个dummy
+
+
+// arrays.sort (默认有小到大排序)
+arrays.sort(总，（元素1，元素2) -> Compare(元素1的XX，元素2的XX))
+
+// 更简易的for loop
+for (int[] x : intervals)
+	
